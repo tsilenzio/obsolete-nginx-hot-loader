00d4c9301772fbcb751550fc46fdbff9fe7a9c05
Taylor Silenzio <me@tsilenz.io>
2018-04-14 00:49:15 -0500

    seperate Ansible from Conf

---
+++ src/Ansible.js
@@ -0,0 +1,29 @@
+import { spawn }  from 'child_process';
+
+// Internal reference to spawned process
+let _child;
+
+// Track if the process is currently running
+let _executing = false;
+
+export default class Ansible {
+  constructor(playbook, ...params) {
+    // Prevent running the command concurrently
+    if(!_executing) {
+      _executing = true;
+      params.unshift(playbook)
+      this.exec(params);
+    }
+  }
+
+  exec(params) {
+    // Update the internal spawn reference;
+    _child = spawn('ansible-playbook', params, {
+      // Pipe the output, including colors, to the console
+      shell: true,
+      stdio: 'inherit'
+    }).on('close', function() {
+      _executing = false;
+    });
+  }
+}
--- src/Conf.js
+++ src/Conf.js
@@ -1,31 +1,8 @@
-const spawn = require('child_process').spawn;
+import Ansible from './Ansible';
 
-let _executing = false;
-
-// External class for handling conf reloading
+//
 module.exports = class Conf {
   reload() {
-    new Ansible();
-  }
-}
-
-// Internal class to handle ansible for nginx conf handling
-class Ansible {
-  constructor() {
-    // Prevent running the command concurrently
-    if(!_executing) {
-      _executing = true;
-      this.exec();
-    }
-  }
-
-  exec() {
-    // Instruct ansible to run the nginx playbook
-    spawn('ansible-playbook', ['playbooks/nginx.yml'], {
-      shell: true,
-      stdio: 'inherit'
-    }).on('close', function() {
-      _executing = false;
-    });
+    new Ansible('playbooks/nginx.yml');
   }
 }
